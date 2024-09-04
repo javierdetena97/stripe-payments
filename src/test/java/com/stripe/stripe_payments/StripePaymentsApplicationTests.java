ed6fc3f3a7c5c5b8f1ed5e8537cc1ad93a2159d0
Javier de Tena <javidetena97@gmail.com>
2024-09-04 22:55:11 +0200

    Add unversioned files

---
+++ src/test/java/com/stripe/stripe_payments/StripePaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stripe.stripe_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripePaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
