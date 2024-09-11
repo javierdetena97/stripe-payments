d9c492951dfa24c5d64be1ce6284c58609fa2f47
Javier de Tena <javidetena97@gmail.com>
2024-09-11 18:40:31 +0200

    Add stripe strategy for checkout session completed

--- src/main/java/com/stripe/stripepayments/repositories/PaymentRepository.java
+++ src/main/java/com/stripe/stripepayments/repositories/PaymentRepository.java
@@ -4,4 +4,5 @@ import com.stripe.stripepayments.commons.entities.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Payment findByPaymentId(String paymentId);
 }
--- src/main/java/com/stripe/stripepayments/strategy/impl/StripeStrategyCheckoutSessionCompleted.java
+++ src/main/java/com/stripe/stripepayments/strategy/impl/StripeStrategyCheckoutSessionCompleted.java
@@ -1,12 +1,22 @@
 package com.stripe.stripepayments.strategy.impl;
 
 import com.stripe.model.Event;
+import com.stripe.model.checkout.Session;
+import com.stripe.stripepayments.commons.entities.Payment;
 import com.stripe.stripepayments.commons.enums.StripeEventEnum;
+import com.stripe.stripepayments.repositories.PaymentRepository;
 import com.stripe.stripepayments.strategy.StripeStrategy;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class StripeStrategyCheckoutSessionCompleted implements StripeStrategy {
+    private final PaymentRepository paymentRepository;
+
+    public StripeStrategyCheckoutSessionCompleted(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
 
     @Override
     public boolean isApplicable(Event event) {
@@ -15,7 +25,24 @@ public class StripeStrategyCheckoutSessionCompleted implements StripeStrategy {
 
     @Override
     public Event process(Event event) {
-        return null;
+        var session = this.deserialize(event);
+        return Optional.of(event)
+                .map(givenEvent -> paymentRepository.findByPaymentId(session.getPaymentIntent()))
+                .map(payment -> setProductId(payment, session.getMetadata().get("product_id")))
+                .map(paymentRepository::save)
+                .map(given -> event)
+                .orElseThrow(() -> new RuntimeException("Error processing"));
+    }
+
+    private Payment setProductId(Payment payment, String productId) {
+        payment.setProductId(productId);
+        payment.setType(StripeEventEnum.CHECKOUT_SESSION_COMPLETED);
+        return payment;
+    }
+
+    private Session deserialize(Event event) {
+        return (Session) event.getDataObjectDeserializer().getObject()
+                .orElseThrow(() -> new RuntimeException("Error deserializing"));
     }
 
 }
