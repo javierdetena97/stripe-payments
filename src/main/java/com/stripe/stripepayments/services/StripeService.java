3829cf4e406bee4ab0c825e67fd910fd6f28e05e
Javier de Tena <javidetena97@gmail.com>
2024-09-14 19:46:04 +0200

    Add createCustomer, createProducts and createPrice methods

--- src/main/java/com/stripe/stripepayments/services/StripeService.java
+++ src/main/java/com/stripe/stripepayments/services/StripeService.java
@@ -1,8 +1,14 @@
 package com.stripe.stripepayments.services;
 
+import com.stripe.model.Customer;
 import com.stripe.model.Event;
+import com.stripe.model.Price;
+import com.stripe.model.Product;
 
 public interface StripeService {
     void manageWebhook(Event event);
     Event constructEvent(String payload, String stripeHeader);
+    Customer createCustomer(String email);
+    Product createProduct(String name);
+    Price createPrice(String productId);
 }
--- src/main/java/com/stripe/stripepayments/services/impl/StripeServiceImpl.java
+++ src/main/java/com/stripe/stripepayments/services/impl/StripeServiceImpl.java
@@ -1,8 +1,16 @@
 package com.stripe.stripepayments.services.impl;
 
+import com.stripe.Stripe;
 import com.stripe.exception.SignatureVerificationException;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
 import com.stripe.model.Event;
+import com.stripe.model.Price;
+import com.stripe.model.Product;
 import com.stripe.net.Webhook;
+import com.stripe.param.CustomerCreateParams;
+import com.stripe.param.PriceCreateParams;
+import com.stripe.param.ProductCreateParams;
 import com.stripe.stripepayments.services.StripeService;
 import com.stripe.stripepayments.strategy.StripeStrategy;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,7 +25,9 @@ public class StripeServiceImpl implements StripeService {
     private final String endpointSecret;
     private final List<StripeStrategy> stripeStrategies;
 
-    public StripeServiceImpl(@Value("${stripe.endpoint.secret}") String endpointSecret, List<StripeStrategy> stripeStrategies) {
+    public StripeServiceImpl(@Value("${stripe.endpoint.secret}") String endpointSecret, List<StripeStrategy> stripeStrategies,
+                             @Value("${stripe.endpoint.key}") String stripeKey) {
+        Stripe.apiKey = stripeKey;
         this.endpointSecret = endpointSecret;
         this.stripeStrategies = Collections.unmodifiableList(stripeStrategies);
     }
@@ -41,6 +51,48 @@ public class StripeServiceImpl implements StripeService {
         try {
             return Webhook.constructEvent(payload, stripeHeader, endpointSecret);
         } catch (SignatureVerificationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Customer createCustomer(String email) {
+        var customerCreateParams = CustomerCreateParams.builder()
+                .setEmail(email)
+                .build();
+
+        try {
+            return Customer.create(customerCreateParams);
+        } catch (StripeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Product createProduct(String name) {
+        var productCreateParams = ProductCreateParams.builder()
+                .setName(name)
+                .setType(ProductCreateParams.Type.SERVICE)
+                .build();
+
+        try {
+            return Product.create(productCreateParams);
+        } catch (StripeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Price createPrice(String productId) {
+        var createPrice = PriceCreateParams.builder()
+                .setCurrency("eur")
+                .setProduct(productId)
+                .setUnitAmount(4000L)
+                .build();
+
+        try {
+            return Price.create(createPrice);
+        } catch (StripeException e) {
             throw new RuntimeException(e);
         }
     }
